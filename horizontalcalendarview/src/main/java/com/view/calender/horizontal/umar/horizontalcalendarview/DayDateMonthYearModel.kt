c519c89bcdc99f289d23046fc5ec7cf969c9b638
Bruce Nishimura <bruceninga@gmail.com>
2019-07-28 13:11:01 -0400

    Improved efficiency by using notifyItemChanged instead of notifyDataset changed Fixed bug where the selected today date wouldn't change at midnight

--- horizontalcalendarview/src/main/java/com/view/calender/horizontal/umar/horizontalcalendarview/DayDateMonthYearModel.kt
+++ horizontalcalendarview/src/main/java/com/view/calender/horizontal/umar/horizontalcalendarview/DayDateMonthYearModel.kt
@@ -18,14 +18,9 @@ class DayDateMonthYearModel(
         val monthNames: Array<String> = arrayOf("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
         val daysOfWeek: Array<String> = arrayOf("Sunday","Monday","Tuesday","Wednesday","Thursday", "Friday","Saturday")
     }
-    val month: String
-        get() = monthNames[localDate.get(Calendar.MONTH)]
-    val year: String
-        get() = localDate.get(Calendar.YEAR).toString()
-    val date: String
-        get() = localDate.get(Calendar.DAY_OF_MONTH).toString()
-    val day: String
-        get() = daysOfWeek[localDate.get(Calendar.DAY_OF_WEEK)-1]
-    val isToday: Boolean
-        get() = (this.localDate.timeInMillis / 1000 / 60 / 60 / 24).toInt() == (Calendar.getInstance().timeInMillis/ 1000 / 60 / 60 / 24).toInt()
+    val month: String = monthNames[localDate.get(Calendar.MONTH)]
+    val year: String = localDate.get(Calendar.YEAR).toString()
+    val date: String = localDate.get(Calendar.DAY_OF_MONTH).toString()
+    val day: String = daysOfWeek[localDate.get(Calendar.DAY_OF_WEEK)-1]
+    fun isToday(): Boolean = (this.localDate.timeInMillis / 1000 / 60 / 60 / 24).toInt() == (Calendar.getInstance().timeInMillis / 1000 / 60 / 60 / 24).toInt()
 }
